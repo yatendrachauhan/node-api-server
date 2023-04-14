9caccbf8c765f7c640cf39ef1fe81255f124a531
Yatendra Chauhan <yatendra.chauhan@nagarro.com>
2023-04-14 16:07:14 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const nedb = require("nedb");
+const rest = require("express-nedb-rest");
+const cors = require("cors");
+
+const app = express();
+
+const datastore = new nedb({ filename: "mycoffeapp.db", autoload: true });
+const restApi = rest();
+restApi.addDatastore('coffees', datastore);
+
+app.use(cors());
+app.use('/', restApi);
+
+app.listen(3000);
